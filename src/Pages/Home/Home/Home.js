32ce06fc1c4cce688a10153033acd6dbb878a521
abir14016 <abir14016@gmail.com>
2022-10-18 09:01:53 +0600

    react router setup

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div className='mx-28'>
+            <Banner></Banner>
+            <h2>Hello</h2>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    themes: [
+      {
+        dark: {
+          "primary": "#f84f55",
+          "secondary": "#df2774",
+          "accent": "#212425",
+          "neutral": "#3D4451",
+          "base-100": "#111111",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+        light: {
+          "primary": "#f84f54",
+          "secondary": "#de2674",
+          "accent": "#f3f6f6",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#0072b1",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        }
+      },
+    ],
   },
   theme: {
     extend: {},
