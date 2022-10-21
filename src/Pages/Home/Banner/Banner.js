2cb67fc4e733c88f9833b06291eeb0de1aea5c93
abir14016 <abir14016@gmail.com>
2022-10-21 22:53:53 +0600

    working on banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen bg-base-100">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" alt='randomImage' />
+        <div className="block md:block lg:flex xl:flex justify-around bg-transparent">
+            <div className='bg-base-100'>
+                <h4 className='text-4xl text-left'>Hello,</h4>
+                <p className='text-4xl text-left'>I'm <span className='text-5xl text-secondary'>Abir Hasan</span></p>
+                <div className='text-5xl text-left mt-3'>
+                    <Typewriter
+                        options={{
+                            strings: [
+                                'Front End Developer',
+                                'Backend Developer',
+                                'Javascript Developer',
+                                'React Developer',
+                                'Full-Strack Developer',
+                            ],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                </div>
+            </div>
+            <div className='bg-base-100'>
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <img src="" alt="abir-1" />
                 </div>
             </div>
         </div>
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,9 +4,7 @@ import Banner from '../Banner/Banner';
 const Home = () => {
     return (
         <div>
-            {/* <Banner></Banner> */}
-            <h2 className='text-base-100'>Hello</h2>
-            {/* <Banner></Banner> */}
+            <Banner></Banner>
         </div>
     );
 };
