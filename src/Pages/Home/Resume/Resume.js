6d05b97e155df38e71c2fb5d1a8847cfb23b5c5b
abir14016 <abir14016@gmail.com>
2022-11-13 03:30:15 +0600

    progressbar animated when scroll with react visibility sensor

--- src/Pages/Home/Resume/Resume.js
+++ src/Pages/Home/Resume/Resume.js
@@ -5,11 +5,12 @@ import abir4 from '../../../assests/images/abir/abir-4.png';
 import Skills from '../Skills/Skills';
 import { ProgressBar } from "react-progressbar-fancy";
 import './Resume.css';
-// import VisibilitySensor from "react-visibility-sensor";
+import VisibilitySensor from "react-visibility-sensor";
+import CountUp from 'react-countup';
 
 
 const Resume = () => {
-    // const [focus, setFocus] = useState(false);
+    const [focus, setFocus] = React.useState(false);
     return (
         <div className='bg-base-100 rounded-2xl mx-2 md:mx-8 lg:mx-28 px-4 md:px-8 lg:px-20 py-4 md:py-8 lg:py-16 my-4 md:my-8 lg:my-16 xl:my-20'>
             <div className='flex justify-start items-center mb-5 md:mb-8 lg:mb-10 xl:mb-12'>
@@ -28,69 +29,200 @@ const Resume = () => {
                 </div>
                 <div className="flex flex-col gap-4 w-full my-8 md:my-0">
                     <h2 className="text-xl text-left font-Poppins font-bold mr-5"><span><FontAwesomeIcon className='text-primary mr-3' icon="fa-solid fa-user-gear" /></span>Working skills</h2>
+
+                    {/* frontend development progress bar */}
                     <div className='mb-2'>
-                        <p className="flex justify-between px-4 pb-2 font-semibold">
-                            <span>Frontend Development</span>
-                            <span>90%</span>
-                        </p>
-                        <ProgressBar
-                            primaryColor='#f84f54'
-                            secondaryColor='#de2674'
-                            hideText
-                            score={90}
-                        />
+                        <VisibilitySensor>
+                            {({ isVisible }) => {
+                                const score = isVisible ? 90 : 0;
+                                return (
+                                    <div>
+                                        <p className="flex justify-between px-4 pb-2 font-semibold">
+                                            <span>Frontend Development</span>
+                                            <span>
+                                                <CountUp start={focus ? 0 : null} end={90} duration={1} redraw={true} suffix="%">
+                                                    {({ countUpRef }) => (
+                                                        <div>
+                                                            <span ref={countUpRef} />
+                                                            <VisibilitySensor
+                                                                onChange={isVisible => {
+                                                                    if (isVisible) {
+                                                                        setFocus(true);
+                                                                    }
+                                                                }}
+                                                            >
+                                                            </VisibilitySensor>
+                                                        </div>
+                                                    )}
+                                                </CountUp>
+                                            </span>
+                                        </p>
+                                        <ProgressBar
+                                            primaryColor='#f84f54'
+                                            secondaryColor='#de2674'
+                                            hideText
+                                            score={score}
+                                        />
+                                    </div>
+                                );
+                            }}
+                        </VisibilitySensor>
                     </div>
 
+                    {/* backend development progress bar */}
                     <div className='mb-2'>
-                        <p className="flex justify-between px-4 pb-2 font-semibold">
-                            <span>Backend Development</span>
-                            <span>70%</span>
-                        </p>
-                        <ProgressBar
-                            primaryColor='#f84f54'
-                            secondaryColor='#de2674'
-                            hideText
-                            score={70}
-                        />
+                        <VisibilitySensor>
+                            {({ isVisible }) => {
+                                const score = isVisible ? 70 : 0;
+                                return (
+                                    <div>
+                                        <p className="flex justify-between px-4 pb-2 font-semibold">
+                                            <span>Backend Development</span>
+                                            <span>
+                                                <CountUp start={focus ? 0 : null} end={70} duration={1} redraw={true} suffix="%">
+                                                    {({ countUpRef }) => (
+                                                        <div>
+                                                            <span ref={countUpRef} />
+                                                            <VisibilitySensor
+                                                                onChange={isVisible => {
+                                                                    if (isVisible) {
+                                                                        setFocus(true);
+                                                                    }
+                                                                }}
+                                                            >
+                                                            </VisibilitySensor>
+                                                        </div>
+                                                    )}
+                                                </CountUp>
+                                            </span>
+                                        </p>
+                                        <ProgressBar
+                                            primaryColor='#f84f54'
+                                            secondaryColor='#de2674'
+                                            hideText
+                                            score={score}
+                                        />
+                                    </div>
+                                );
+                            }}
+                        </VisibilitySensor>
                     </div>
 
+                    {/* ui/ux design progress bar */}
                     <div className='mb-2'>
-                        <p className="flex justify-between px-4 pb-2 font-semibold">
-                            <span>Ui/Ux Design</span>
-                            <span>85%</span>
-                        </p>
-                        <ProgressBar
-                            primaryColor='#f84f54'
-                            secondaryColor='#de2674'
-                            hideText
-                            score={85}
-                        />
+                        <VisibilitySensor>
+                            {({ isVisible }) => {
+                                const score = isVisible ? 85 : 0;
+                                return (
+                                    <div>
+                                        <p className="flex justify-between px-4 pb-2 font-semibold">
+                                            <span>Ui/Ux Design</span>
+                                            <span>
+                                                <CountUp start={focus ? 0 : null} end={85} duration={1} redraw={true} suffix="%">
+                                                    {({ countUpRef }) => (
+                                                        <div>
+                                                            <span ref={countUpRef} />
+                                                            <VisibilitySensor
+                                                                onChange={isVisible => {
+                                                                    if (isVisible) {
+                                                                        setFocus(true);
+                                                                    }
+                                                                }}
+                                                            >
+                                                            </VisibilitySensor>
+                                                        </div>
+                                                    )}
+                                                </CountUp>
+                                            </span>
+                                        </p>
+                                        <ProgressBar
+                                            primaryColor='#f84f54'
+                                            secondaryColor='#de2674'
+                                            hideText
+                                            score={score}
+                                        />
+                                    </div>
+                                );
+                            }}
+                        </VisibilitySensor>
                     </div>
 
+                    {/* app development progress bar */}
                     <div className='mb-2'>
-                        <p className="flex justify-between px-4 pb-2 font-semibold">
-                            <span>App Development</span>
-                            <span>50%</span>
-                        </p>
-                        <ProgressBar
-                            primaryColor='#f84f54'
-                            secondaryColor='#de2674'
-                            hideText
-                            score={50}
-                        />
+                        <VisibilitySensor>
+                            {({ isVisible }) => {
+                                const score = isVisible ? 50 : 0;
+                                return (
+                                    <div>
+                                        <p className="flex justify-between px-4 pb-2 font-semibold">
+                                            <span>App Development</span>
+                                            <span>
+                                                <CountUp start={focus ? 0 : null} end={50} duration={1} redraw={true} suffix="%">
+                                                    {({ countUpRef }) => (
+                                                        <div>
+                                                            <span ref={countUpRef} />
+                                                            <VisibilitySensor
+                                                                onChange={isVisible => {
+                                                                    if (isVisible) {
+                                                                        setFocus(true);
+                                                                    }
+                                                                }}
+                                                            >
+                                                            </VisibilitySensor>
+                                                        </div>
+                                                    )}
+                                                </CountUp>
+                                            </span>
+                                        </p>
+                                        <ProgressBar
+                                            primaryColor='#f84f54'
+                                            secondaryColor='#de2674'
+                                            hideText
+                                            score={score}
+                                        />
+                                    </div>
+                                );
+                            }}
+                        </VisibilitySensor>
                     </div>
 
+                    {/* problem solving progress bar */}
                     <div>
-                        <p className="flex justify-between px-4 pb-2 font-semibold">
-                            <span>Problem Solving</span>
-                            <span>40%</span>
-                        </p>
-                        <ProgressBar
-                            primaryColor='#f84f54'
-                            secondaryColor='#de2674'
-                            hideText
-                            score={40}
-                        />
+                        <VisibilitySensor>
+                            {({ isVisible }) => {
+                                const score = isVisible ? 40 : 0;
+                                return (
+                                    <div>
+                                        <p className="flex justify-between px-4 pb-2 font-semibold">
+                                            <span>Problem Solving</span>
+                                            <span>
+                                                <CountUp start={focus ? 0 : null} end={40} duration={1} redraw={true} suffix="%">
+                                                    {({ countUpRef }) => (
+                                                        <div>
+                                                            <span ref={countUpRef} />
+                                                            <VisibilitySensor
+                                                                onChange={isVisible => {
+                                                                    if (isVisible) {
+                                                                        setFocus(true);
+                                                                    }
+                                                                }}
+                                                            >
+                                                            </VisibilitySensor>
+                                                        </div>
+                                                    )}
+                                                </CountUp>
+                                            </span>
+                                        </p>
+                                        <ProgressBar
+                                            primaryColor='#f84f54'
+                                            secondaryColor='#de2674'
+                                            hideText
+                                            score={score}
+                                        />
+                                    </div>
+                                );
+                            }}
+                        </VisibilitySensor>
                     </div>
                 </div>
             </div>
