5f3b1eacfd16c6e9f20d722d6a278185536dd299
abir14016 <abir14016@gmail.com>
2023-01-07 00:48:10 +0600

    courses section with swipper slider

--- src/Pages/Home/Home/Courses/Courses.js
+++ src/Pages/Home/Home/Courses/Courses.js
@@ -20,6 +20,7 @@ const Courses = () => {
             id: 1,
             title: "web",
             subTitle: "complete web development",
+            details: "Full-stack web development course organized by PROGRAMMING HERO. It takes 6 to 8 months to complete the course.",
             picture: web,
             topics: ["HTML", "CSS", "Javascript", "React.JS", "Express.JS", "Firebase", "Tailwind", "Bootstrap"],
             status: "completed",
@@ -29,6 +30,7 @@ const Courses = () => {
             id: 2,
             title: "SCIC",
             subTitle: "job hunting battlefield",
+            details: "Only the students who have completed main web course of PROGRAMMING HERO ontime and obtain at least average 50% marks for all assignments can access the course.",
             picture: scic,
             topics: ["Resume", "Interview Preparation", "LinkedIn", "Github"],
             status: "completed",
@@ -38,6 +40,7 @@ const Courses = () => {
             id: 3,
             title: "Node-Mongo",
             subTitle: "advanced crash course",
+            details: "Only the students who have completed main web course and SCIC both of PROGRAMMING HERO can access the course. This course mainly focuses on backend development",
             picture: nodeMongo,
             topics: ["Node.JS", "MongoDB", "Javascript"],
             status: "running",
@@ -47,8 +50,9 @@ const Courses = () => {
             id: 4,
             title: "Redux",
             subTitle: "beginner to advance",
+            details: "Only the students who have completed main web course and SCIC both of PROGRAMMING HERO can access the course. This course mainly focuses on state management of react",
             picture: redux,
-            topics: ["Redux", "Redux Toolkit"],
+            topics: ["Redux", "Redux Thunk", "Redux Toolkit"],
             status: "running",
             icon: "fa-solid fa-person-running"
         },
@@ -56,6 +60,7 @@ const Courses = () => {
             id: 5,
             title: "Advanced Javascript",
             subTitle: "excilency in professional carrer",
+            details: "Only the students who have got hired for a job or internship can access the course. This course mainly focuses on deep learning of javascript",
             picture: js,
             topics: ["Advance Js", "Advance Github"],
             status: "running",
@@ -65,6 +70,7 @@ const Courses = () => {
             id: 6,
             title: "LEDP",
             subTitle: "Learning & earning development project",
+            details: "This course is organized by the ICT ministry of Bangladesh and foucuses on freelancing.",
             picture: ledp,
             topics: ["HHML", "CSS", "Bootstrap", "Freelancing"],
             status: "completed",
@@ -73,11 +79,11 @@ const Courses = () => {
     ]
     return (
         <div>
-            <h2 className="text-3xl text-left font-Poppins font-bold mr-5"><span><FontAwesomeIcon className='text-primary mr-3 wave' icon="fa-solid fa-screwdriver-wrench" /></span>Courses</h2>
+            <h2 className="text-3xl text-left font-Poppins font-bold mr-5"><span><FontAwesomeIcon className='text-primary mr-3 wave' icon="fa-solid fa-person-chalkboard" /></span>Courses</h2>
 
             <Swiper
                 effect={"coverflow"}
-                autoHeight={true}
+                // autoHeight={true}
                 grabCursor={true}
                 centeredSlides={true}
                 // autoplay={true}
@@ -128,10 +134,10 @@ const Courses = () => {
                                 <div>
                                     <p className="text-left text-xs text-secondary">{course.subTitle}</p>
                                 </div>
-                                <p className="text-left text-slate-400">If a dog chews shoes whose shoes does he choose?</p>
+                                <p className="text-left">{course.details}</p>
                                 <div className="card-actions justify-start">
                                     {
-                                        course.topics.map(topic => <div key={topic} className="badge badge-outline">{topic}</div>)
+                                        course.topics.map(topic => <div key={topic} className="badge badge-neutral">{topic}</div>)
                                     }
                                 </div>
                             </div>
